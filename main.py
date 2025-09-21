94b5e351784a31f6a31bd14c4f1bf3ceb1c29f6f
xam <glebmatveev34@gmail.com>
2025-09-21 21:15:08 +1000

    Added function descriptions and finished GetDepth() function

--- main.py
+++ main.py
@@ -7,16 +7,102 @@ import numpy as np
 import xarray as xr
 import gsw
 
+
+def seawater_density_unesco(salinity, temperature, pressure):
+    """
+    Расчет плотности морской воды по уравнению UNESCO 1981
+    """
+    # Константы
+    a0 = 999.842594
+    a1 = 6.793952e-2
+    a2 = -9.095290e-3
+    a3 = 1.001685e-4
+    a4 = -1.120083e-6
+    a5 = 6.536332e-9
+
+    b0 = 8.24493e-1
+    b1 = -4.0899e-3
+    b2 = 7.6438e-5
+    b3 = -8.2467e-7
+    b4 = 5.3875e-9
+
+    c0 = -5.72466e-3
+    c1 = 1.0227e-4
+    c2 = -1.6546e-6
+
+    d0 = 4.8314e-4
+
+    # Плотность чистой воды при атмосферном давлении
+    t = temperature
+    rho_w = a0 + a1 * t + a2 * t ** 2 + a3 * t ** 3 + a4 * t ** 4 + a5 * t ** 5
+
+    # Поправка на соленость при атмосферном давлении
+    s = salinity
+    rho_0 = (rho_w +
+             (b0 + b1 * t + b2 * t ** 2 + b3 * t ** 3 + b4 * t ** 4) * s +
+             (c0 + c1 * t + c2 * t ** 2) * s ** 1.5 +
+             d0 * s ** 2)
+
+    # Сжимаемость (поправка на давление)
+    p = pressure  # в барах
+    k0 = 19652.21 + 148.4206 * t - 2.327105 * t ** 2 + 1.360477e-2 * t ** 3 - 5.155288e-5 * t ** 4
+    k1 = 54.6746 - 0.603459 * t + 1.09987e-2 * t ** 2 - 6.1670e-5 * t ** 3
+    k2 = 7.944e-2 + 1.6483e-2 * t - 5.3009e-4 * t ** 2
+
+    k = (k0 + k1 * s + k2 * s ** 1.5 +
+         (3.239908 + 1.43713e-3 * t + 1.16092e-4 * t ** 2 - 5.77905e-7 * t ** 3) * p +
+         (2.2838e-3 - 1.0981e-5 * t - 1.6078e-6 * t ** 2) * p * s +
+         (1.91075e-4) * p * s ** 1.5 +
+         (8.50935e-5 - 6.12293e-6 * t + 5.2787e-8 * t ** 2) * p ** 2 +
+         (-9.9348e-7 + 2.0816e-8 * t + 9.1697e-10 * t ** 2) * p ** 2 * s)
+
+    # Плотность при заданном давлении
+    rho = rho_0 / (1 - p / k)
+
+    return rho
+
+def getDepth_unesco(pressure, salinity, temperature, latitude):
+    """
+      Расчет глубины с учетом плотности, зависящей от T, S, P
+    """
+    # Расчет гравитации
+    lat_rad = np.radians(latitude)
+    g = 9.780318 * (1 + 0.0053024 * np.sin(lat_rad) ** 2
+                    - 0.0000058 * np.sin(2 * lat_rad) ** 2)
+
+    # Расчет плотности на данной глубине (итеративно)
+    depth_guess = pressure  # начальное приближение
+    tolerance = 0.001
+    max_iter = 10
+
+    for i in range(max_iter):
+        # Оцениваем давление на этой глубине
+        p_estimated = pressure
+
+        # Рассчитываем плотность
+        rho = seawater_density_unesco(salinity, temperature, p_estimated)
+
+        # Пересчитываем глубину
+        depth_new = pressure * 10000 / (rho * g)
+
+        # Проверяем сходимость
+        if abs(depth_new - depth_guess) < tolerance:
+            break
+
+        depth_guess = depth_new
+
+    return depth_new
+
 def getDepth(pres, lat):
     """
-        Processes the input netCDF file
+        Calculating depth by pressure and latitude
 
         Args:
             pres: pressure from netCDF.
             lat: latitude from netCDF.
 
         Returns:
-           float Depth calculated from the input netCDF.
+           Depth.
 
     """
     depth = gsw.z_from_p(pres, lat)
@@ -92,7 +178,8 @@ def csv_gen(filedir, finish_dict):
                     lat = "{0:.3f}".format(item.lat)
                 if item.lon != "None":
                     lon = "{0:.3f}".format(item.lon)
-                depth =  "{0:.3f}".format(item.depth)
+                if item.depth != "None":
+                    depth =  "{0:.3f}".format(item.depth)
                 if item.temp != "None":
                     temp = "{0:.3f}".format(item.temp)
                 if item.pres != "None":
@@ -151,7 +238,8 @@ def process_file(input_filename, output_path, lon_min = -180, lon_max = 180, lat
                         finish_dict[str(dt_data[i]).split("T")[0]] = []
                     finish_dict[str(dt_data[i]).split("T")[0]].append(
                         Record(lats[i], lons[i],
-                               getDepth(pres[i, j], lats[i]),
+                               #getDepth(pres[i, j], lats[i]),
+                               getDepth_unesco(pres[i,j], psal[i,j],temp[i,j], lats[i]),
                                temp[i,j], pres[i, j], psal[i, j],
                                str(dt_data[i]).split('.')[0], platform_number[i], cycle_number[i]))
     ds.close()
@@ -161,7 +249,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Программа выборки данных из Argo NetCDF")
     parser.add_argument(
         "--input_dir", "-i",
-        required=True,
+        # required=True,
+        default="./",
         help="Путь к директории с файлами netcdf."
     )
     parser.add_argument(
